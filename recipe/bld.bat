652ff40c47d5f62ede9f02eb7b966e5c98b895bb
shadow_walker <shadowwalkersb@gmail.com>
2016-08-31 22:05:31 -0500

    Initial commit of the gorgon-legacy feedstock.

---
+++ recipe/bld.bat
@@ -0,0 +1,17 @@
+SET builddir=%SRC_DIR%\..\build_dir
+
+rmdir /s /q %builddir%
+if errorlevel 1 exit 1
+mkdir  %builddir%
+if errorlevel 1 exit 1
+cd %builddir%
+if errorlevel 1 exit 1
+
+cmake %SRC_DIR% -G "%CMAKE_GENERATOR%"
+if errorlevel 1 exit 1
+
+cmake --build %builddir% --config Release --target Legacy
+if errorlevel 1 exit 1
+
+"%PYTHON%" setup_legacy.py install
+if errorlevel 1 exit 1
