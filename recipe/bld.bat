90a28e262ea2d601c1866701401424a9058b8c9f
shadow_walker <shadowwalkersb@gmail.com>
2016-09-03 00:31:15 -0500

    add cmake generator

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,11 @@
 SET builddir=%SRC_DIR%\..\build_dir
 
+if "%ARCH%" == "64" (
+  set cmake_gen="Visual Studio 14 2015 Win64"
+) else (
+  set cmake_gen="Visual Studio 14 2015"
+)
+
 rmdir /s /q %builddir%
 if errorlevel 1 exit 1
 mkdir  %builddir%
@@ -7,7 +13,7 @@ if errorlevel 1 exit 1
 cd %builddir%
 if errorlevel 1 exit 1
 
-cmake %SRC_DIR%
+cmake %SRC_DIR% -G %cmake_gen%
 if errorlevel 1 exit 1
 
 cmake --build %builddir% --config Release --target Legacy
