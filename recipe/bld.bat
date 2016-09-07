4122cf7e1f5c8cc72852e3b30ead6f8dc440c0e1
shadow_walker <shadowwalkersb@gmail.com>
2016-09-07 14:04:28 -0500

    use system default VC, not CMAKE_GENERATOR

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,7 +7,7 @@ if errorlevel 1 exit 1
 cd %builddir%
 if errorlevel 1 exit 1
 
-cmake %SRC_DIR% -G "%CMAKE_GENERATOR%"
+cmake %SRC_DIR%
 if errorlevel 1 exit 1
 
 cmake --build %builddir% --config Release --target Legacy
